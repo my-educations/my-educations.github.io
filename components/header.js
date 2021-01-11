87058866663890a6049db25f7a85c64310413e8c
Steven Cheng <cheng.jiechao@yan3k.com>
2021-01-11 15:42:39 +0800

    school

--- components/header.js
+++ components/header.js
@@ -106,8 +106,8 @@ headerTemplate.innerHTML = `
                 <span class="caret"></span>
               </button>
               <div class="dropdown-content">
-                <a href="my-cn.html">马来西亚</a>
-                <a href="my-cn.html">泰国</a>
+                <a href="school-my-public-cn.html.html">马来西亚</a>
+                <a href="school-my-public-cn.html.html">泰国</a>
               </div>
             </li>
             <li class="dropdown">
