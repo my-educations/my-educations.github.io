71ce35ada1790f1e64863023ca031ac6caf072ae
Steven Cheng <cheng.jiechao@yan3k.com>
2021-01-11 21:58:32 +0800

    school

--- components/header.js
+++ components/header.js
@@ -1,11 +1,6 @@
 const headerTemplate = document.createElement('template');
 headerTemplate.innerHTML = `
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <link rel="stylesheet" href="static/w3.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="static/styles.css">
-  <script src="js/jquery.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
+  
   <style>
     .affix {
       top: 0;
