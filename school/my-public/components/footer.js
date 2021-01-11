e05a4a8f7ad256961009073ce4ac76283aa015f8
Steven Cheng <cheng.jiechao@yan3k.com>
2021-01-11 15:48:28 +0800

    school

--- school/my-public/components/footer.js
+++ school/my-public/components/footer.js
@@ -3,7 +3,7 @@ footerTemplate.innerHTML = `
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="../../../../../../../../../static/styles.css">
+  <link rel="stylesheet" href="../../static/styles.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <style>
@@ -32,17 +32,17 @@ footerTemplate.innerHTML = `
           <div class="col-sm-4">
           </div>
           <div class="col-sm-4 text-center">
-            <img src="../../../img/about/wechat-sm.jpg" style="width:60px">
+            <img src="../../img/about/wechat-sm.jpg" style="width:60px">
           </div>
           <div class="col-sm-4 text-center">
-            <img src="../../../img/about/wechat-bm.jpg" style="width:60px">
+            <img src="../../img/about/wechat-bm.jpg" style="width:60px">
           </div>
         </div>
       </div>
 
       <div class="col-sm-6 text-center">
         <div class="row">
-          <img src="../../../img/about/logo.png" style="width:100px">
+          <img src="../../img/about/logo.png" style="width:100px">
         </div>
       </div>
 
@@ -59,10 +59,10 @@ footerTemplate.innerHTML = `
         </div>
         <div class="row">
           <div class="col-sm-4 text-center">
-            <img src="../../../img/about/wechat.jpg" style="width:60px">
+            <img src="../../img/about/wechat.jpg" style="width:60px">
           </div>
           <div class="col-sm-4 text-center">
-            <img src="../../../img/about/qq.jpg" style="width:60px">
+            <img src="../../img/about/qq.jpg" style="width:60px">
           </div>
           <div class="col-sm-4">
           </div>
@@ -78,17 +78,17 @@ footerTemplate.innerHTML = `
           </div>
           <div class="col-sm-8">
             <a href="index.html">
-              <img src="../../../img/about/logo.png" style="width:60px">
+              <img src="../../img/about/logo.png" style="width:60px">
             </a>
           </div>
         </div>
       </div>
       <div class="col-sm-6 text-center">
         <div class="row">
-          <a href="../../../faq-cn.html">常见问题</a>&nbsp;&nbsp;
-          <a href="../../../partner-cn.html">合作推广</a>&nbsp;&nbsp;
-          <a href="../../../contact-cn.html">意见反馈</a>&nbsp;&nbsp;
-          <a href="../../../about-cn.html">关于我们</a>
+          <a href="../../faq-cn.html">常见问题</a>&nbsp;&nbsp;
+          <a href="../../partner-cn.html">合作推广</a>&nbsp;&nbsp;
+          <a href="../../contact-cn.html">意见反馈</a>&nbsp;&nbsp;
+          <a href="../../about-cn.html">关于我们</a>
         </div>
         <div class="row">
           <p>&copy; <script>document.write(new Date().getFullYear())</script> StudyMax.&nbsp;All rights reserved.</p>
