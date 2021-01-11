b3f77bff6473a32cf756ec2fcbb57f194baaba8d
Steven Cheng <cheng.jiechao@yan3k.com>
2021-01-11 22:41:08 +0800

    school

--- components/footer.js
+++ components/footer.js
@@ -1,11 +1,11 @@
 const footerTemplate = document.createElement('template');
 footerTemplate.innerHTML = `
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <link rel="stylesheet" href="../static/w3.css">
+  <link rel="stylesheet" href="static/w3.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="../static/styles.css">
-  <script src="../js/jquery.min.js"></script>
-  <script src="../js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="static/styles.css">
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
   <style>
     .bg-footer {
       background-color: #f2f2f2;
