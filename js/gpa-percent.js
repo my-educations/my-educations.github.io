284a05fe8f60b0284b98cfe2dce5d911824672f9
Steven Cheng <cheng.jiechao@yan3k.com>
2021-01-15 20:32:20 +0800

    tool

--- js/gpa-percent.js
+++ js/gpa-percent.js
@@ -7,51 +7,51 @@ function gpacalc() {
 
   // define valid grades and their values
   var grcount = 12;
-  gr[0] = "A+";
+  gr[0] = 90;
   cr[0] = 4;
-  gr[1] = "A";
+  gr[1] = 80;
   cr[1] = 4;
-  gr[2] = "A-";
+  gr[2] = 75;
   cr[2] = 3.7;
-  gr[3] = "B+";
+  gr[3] = 70;
   cr[3] = 3.3;
-  gr[4] = "B";
+  gr[4] = 65;
   cr[4] = 3;
-  gr[5] = "B-";
+  gr[5] = 60;
   cr[5] = 2.7;
-  gr[6] = "C+";
+  gr[6] = 55;
   cr[6] = 2.3;
-  gr[7] = "C";
+  gr[7] = 50;
   cr[7] = 2;
-  gr[8] = "C-";
+  gr[8] = 45;
   cr[8] = 1.7;
-  gr[9] = "D+";
+  gr[9] = 40;
   cr[9] = 1.3;
-  gr[10] = "D";
+  gr[10] = 35;
   cr[10] = 1;
-  gr[11] = "F";
-  cr[11] = 0;
+  gr[10] = 0;
+  cr[10] = 0;
   // retrieve user input
-  ingr[0] = document.GPACalculator.gr1.value;
-  ingr[1] = document.GPACalculator.gr2.value;
-  ingr[2] = document.GPACalculator.gr3.value;
-  ingr[3] = document.GPACalculator.gr4.value;
-  ingr[4] = document.GPACalculator.gr5.value;
-  ingr[5] = document.GPACalculator.gr6.value;
-  ingr[6] = document.GPACalculator.gr7.value;
-  ingr[7] = document.GPACalculator.gr8.value;
-  ingr[8] = document.GPACalculator.gr9.value;
-  ingr[9] = document.GPACalculator.gr10.value;
-  incr[0] = document.GPACalculator.cr1.value;
-  incr[1] = document.GPACalculator.cr2.value;
-  incr[2] = document.GPACalculator.cr3.value;
-  incr[3] = document.GPACalculator.cr4.value;
-  incr[4] = document.GPACalculator.cr5.value;
-  incr[5] = document.GPACalculator.cr6.value;
-  incr[6] = document.GPACalculator.cr7.value;
-  incr[7] = document.GPACalculator.cr8.value;
-  incr[8] = document.GPACalculator.cr9.value;
-  incr[9] = document.GPACalculator.cr10.value;
+  ingr[0] = document.GPAPercent.gr1.value;
+  ingr[1] = document.GPAPercent.gr2.value;
+  ingr[2] = document.GPAPercent.gr3.value;
+  ingr[3] = document.GPAPercent.gr4.value;
+  ingr[4] = document.GPAPercent.gr5.value;
+  ingr[5] = document.GPAPercent.gr6.value;
+  ingr[6] = document.GPAPercent.gr7.value;
+  ingr[7] = document.GPAPercent.gr8.value;
+  ingr[8] = document.GPAPercent.gr9.value;
+  ingr[9] = document.GPAPercent.gr10.value;
+  incr[0] = document.GPAPercent.cr1.value;
+  incr[1] = document.GPAPercent.cr2.value;
+  incr[2] = document.GPAPercent.cr3.value;
+  incr[3] = document.GPAPercent.cr4.value;
+  incr[4] = document.GPAPercent.cr5.value;
+  incr[5] = document.GPAPercent.cr6.value;
+  incr[6] = document.GPAPercent.cr7.value;
+  incr[7] = document.GPAPercent.cr8.value;
+  incr[8] = document.GPAPercent.cr9.value;
+  incr[9] = document.GPAPercent.cr10.value;
 
   // Calculate GPA
   var allgr =0;
@@ -67,7 +67,7 @@ function gpacalc() {
           var validgrcheck = 0;
           for (var xx = 0; xx < grcount; xx++)
                   {
-                  if (ingr[x] == gr[xx])
+                  if (ingr[x] >= gr[xx])
                           {
                           allgr = allgr + (parseInt(incr[x],10) * cr[xx]);
                           allcr = allcr + parseInt(incr[x],10);
@@ -77,7 +77,7 @@ function gpacalc() {
                   }
           if (validgrcheck == 0)
                   {
-                  alert("科目" + eval(x +  1) + "输入的分数格式有误！ 请输入标准的分数等级，例如：A+、A 或 B+");
+                  alert("科目" + eval(x +  1) + "输入的分数格式有误！ 请输入标准的分数，例如：95、88或75");
                   return 0;
                   }
           }
@@ -96,7 +96,7 @@ function gpacalc() {
 
 
 // When the user clicks anywhere outside of the modal, close it
-var modal = document.getElementById('gpa-calculator');
+var modal = document.getElementById('gpa-percent');
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
