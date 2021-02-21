391259fbd8154c69824022dabd0754721156f5f9
Steven Cheng <cheng.jiechao@yan3k.com>
2021-02-21 12:16:34 +0800

    index

--- js/tab-vertical.js
+++ js/tab-vertical.js
@@ -1,4 +1,4 @@
-function openProgram(evt, cityName) {
+function openProgram(evt, programName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
@@ -8,6 +8,6 @@ function openProgram(evt, cityName) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(programName).style.display = "block";
   evt.currentTarget.className += " active";
 }
