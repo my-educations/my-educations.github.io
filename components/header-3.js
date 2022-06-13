19935e26749451d48d6fa2e17858297c1e69ddc6
Stephen Cheng <stephencheng@Stephens-MacBook-Air.local>
2022-06-13 14:39:13 +0800

    0613

--- components/header-3.js
+++ components/header-3.js
@@ -68,7 +68,7 @@ headerTemplate.innerHTML = `
               <span class="caret"></span>
             </button>
             <ul class="dropdown-content">
-              <li><a href="../../skill_list/skill-living-cn.html">留学指南</a></li>
+              <li><a href="../../skill_list/skill-living-cn.html">本地指南</a></li>
               <li><a href="../../skill_list/skill-ranking-cn.html">学校排名</a></li>
               <li><a href="../../skill_list/language-cn.html">语言学习</a></li>
               <li><a href="../../testimonial-cn.html">offer榜</a></li>
