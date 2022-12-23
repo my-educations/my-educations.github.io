6a68c2fa170d93a331ae5a19e6500c56206f07eb
Stephen Cheng <jetrobert19@gmail.com>
2022-12-23 09:50:54 -0500

    1223

--- components/footer.js
+++ components/footer.js
@@ -111,7 +111,7 @@ footerTemplate.innerHTML = `
           <div class="col-sm-8">
             <a href="https://www.youtube.com/channel/UCasfXLwDL6xoDfF-00co9tQ" class="fa fa-youtube" target="_blank"></a>
             <a href="https://www.facebook.com/study2max" class="fa fa-facebook" target="_blank"></a>
-            <a href="https://www.instagram.com/studymax.malaysia/" class="fa fa-instagram" target="_blank"></a>
+            <a href="https://www.instagram.com/stumax.academy/" class="fa fa-instagram" target="_blank"></a>
             <a href="https://www.linkedin.com/company/study2max/" class="fa fa-linkedin" target="_blank"></a>
             <a href="https://twitter.com/study2max/" class="fa fa-twitter" target="_blank"></a>
           </div>
