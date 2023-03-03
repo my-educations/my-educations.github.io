6fad34ae573ccf122f30a475845ea8c5aa9c6f61
Stephen Cheng <jetrobert19@gmail.com>
2023-03-03 15:32:07 -0500

    0303

--- components/header-en.js
+++ components/header-en.js
@@ -60,13 +60,13 @@ headerTemplate.innerHTML = `
               <span class="caret"></span>
             </button>
             <ul class="dropdown-content">
-              <li><a href="testimonial-cn.html">Offer</a></li>
+              <li><a href="web-cn.html">Websites</a></li>
+              <li><a href="https://discord.com/invite/YPKu6eakHs" target="_blank">Community</a></li>
               <li><a href="skill_list/skill-living-cn.html">Local</a></li>
               <li><a href="skill_list/skills-cn.html">Skills</a></li>
               <li><a href="skill_list/skill-ranking-cn.html">Ranking</a></li>
               <li><a href="skill_list/language-cn.html">Language</a></li>
-              <li><a href="web-cn.html">Websites</a></li>
-              <li><a href="https://discord.com/invite/YPKu6eakHs" target="_blank">Community</a></li>
+              <li><a href="testimonial-cn.html">Offer</a></li>
               <li><a href="faq-cn.html">FAQ</a></li>
               <li><a href="contact-cn.html" target="_blank">Contact us</a></li>
             </ul>
