35fea7a38073907b741cdf3e9d8e943b6f29c9f7
Stephen Cheng <jetrobert19@gmail.com>
2023-12-15 12:41:40 -0500

    1215

--- components/header-2.js
+++ components/header-2.js
@@ -61,15 +61,15 @@ headerTemplate.innerHTML = `
             </button>
             <ul class="dropdown-content">
               <li><a href="https://discord.com/invite/YPKu6eakHs" target="_blank">Community</a></li>
-              <li><a href="../contact-cn.html" target="_blank">Contact us</a></li>
-              <li><a href="../faq-cn.html">FAQ</a></li>
-              <li><a href="../skill_list/language-cn.html">Language</a></li>
               <li><a href="../skill_list/skill-living-cn.html">Local Guide</a></li>
-              <li><a href="https://eastwestlife.com/" target="_blank">Life</a></li>
+              <li><a href="https://eastwestlife.com/" target="_blank">Explore World</a></li>
               <li><a href="../testimonial-cn.html">offer</a></li>
               <li><a href="../skill_list/skills-cn.html">Skills</a></li>
               <li><a href="../skill_list/university-major-cn.html">Univ/Major</a></li>
+              <li><a href="../skill_list/language-cn.html">Language</a></li>
               <li><a href="../web-cn.html">Websites</a></li>
+              <li><a href="../faq-cn.html">FAQ</a></li>
+              <li><a href="../contact-cn.html" target="_blank">Contact us</a></li>
             </ul>
           </li>
         </ul>
